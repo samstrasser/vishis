9b61816c7a088ad421c289d6f122e6431e80769d
sam.strasser <sam.strasser@2eca556a-c63e-0410-9520-154fc19a35bf>
2007-12-26 04:32:47 +0000

    Adding blank nav panel file

---
+++ trunk/js/nav.js
@@ -0,0 +1,21 @@
+/**
+ * nav.js
+ * Nav
+ * Topic
+ */
+ 
+ /**
+  * @class Nav
+  */
+ function Nav(){
+ 
+ }
+ 
+ 
+ /** 
+  * @class Topic
+  * this is just the internal data fields
+  */
+function Topic(){
+
+}
