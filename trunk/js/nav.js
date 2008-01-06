7e633c74ef70f72ca2f65afbf2bf655ae1ea9db9
sam.strasser <sam.strasser@2eca556a-c63e-0410-9520-154fc19a35bf>
2008-01-06 17:05:08 +0000

    Added Topic, TopicList classes and their first-draft structures.

--- trunk/js/nav.js
+++ trunk/js/nav.js
@@ -6,16 +6,153 @@
  
  /**
   * @class Nav
-  */
- function Nav(){
+  **/
+function Nav(navElt){
+	if(!navElt){
+		navElt = document.getElementId('nav-panel');
+	}
+	this.elt = navElt;
+	
+	this.currentTopics = new TopicList('currently-viewing');
+	this.currentTopics.attachToElt(this.elt);
+	
+	this.recentTopics  = new TopicList('recently-viewed');
+	this.recentTopics.attachToElt(this.elt);
+	
+	this.searchBox = {
+		textField: 'todo',
+		searchResultsDiv: 'todo'
+	}; // todo: search box
+}
  
- }
- 
- 
- /** 
-  * @class Topic
-  * this is just the internal data fields
-  */
-function Topic(){
+function TopicList(id, type){
+	this.topics = new Array();
 
+	// Initialize the dom elt
+	this.elt = document.createElement('div');
+	this.elt.setAttribute('id', id);
+	this.elt.setAttribute('class', 'topic-list');
+}
+
+TopicList.prototype.maxLength = 15;
+
+TopicList.prototype.attachTo = function(id){
+	this.attachToElt(document.getElementById(id));
+}
+
+TopicList.prototype.attachToElt = function(elt){
+	elt.appendChild(this.elt);
+}
+
+TopicList.prototype.add = function(topic){
+	if(topic.getId() in this.topics){
+		console.warn('Topic added to list but its already there');
+	}
+	
+	// Save the topic in the list
+	this.topics[topic.id] = topic;
+
+	// Add the topic in the dom
+	this.elt.appendChild(topic.getRootElement());
+}
+
+TopicList.prototype.getTopic = function(id){
+	if(id in this.topics){
+		return this.topics[id];
+	}else{
+		return false;
+	}
+}
+
+TopicList.prototype.getAllTopics = function(){
+	return this.topics;
+}
+
+TopicList.prototype.getLength = function(){
+	return this.getAllTopics.length;
+}
+
+TopicList.prototype.remove = function(topicId){
+	// todo
+	// first detach topic from the parent element
+	
+	// todo
+	// then delete it from the list of topics
+}
+
+// clears the list of all toipics
+//optionally passes the topics to another list
+TopicList.prototype.clear = function(transferTo){
+	var allTopics = this.getAllTopics();
+	for(var topicId in allTopics){
+		var removed = this.remove(allTopics[topicId]);
+		if(transferTo){
+			// add topic to the new list
+			transferTo.add(removed);
+		}
+		
+	}
+}
+
+
+/** 
+ * @class Topic
+ **/
+function Topic(node, displayType){
+	this.id = 0;
+	
+	for(var k in node){
+		this[k] = node[k];
+	}
+
+	this.elt = document.createElement('div');	
+	
+	if(!displayType){
+		displayType = 'current';
+	}
+	this.setDisplayType(displayType);
+	
+	var cbox = document.createElement('input');
+	cbox.setAttribute('type','checkbox');
+	cbox.setAttribute('checkbox','checkbox');
+	this.elt.appendChild(cbox);
+	
+	var add = document.createElement('span');
+	add.setAttribute('class', 'add');
+	add.innerHTML = '+';
+	this.elt.appendChild(add);
+	
+	var title = document.createElement('span');
+	title.setAttribute('class', 'title');
+	title.innerHTML = 'todo: this.title';
+	this.elt.appendChild(title);
+	
+	var color = document.createElement('div');
+	color.setAttribute('class', 'swatch');
+	color.innerHTML = ' ';
+	color.style.backgroundColor = '#aaaaaa'; // todo: this.color
+	this.elt.appendChild(color);
+	
+	var remove = document.createElement('span');
+	remove.setAttribute('class', 'remove');
+	remove.innerHTML = 'X';
+	this.elt.appendChild(remove);
+}
+
+Topic.prototype.getId = function(){
+	return this.id;
+}
+
+Topic.prototype.getRootElement = function(){
+	return this.elt;
+}
+
+Topic.prototype.hide = function(){
+}
+
+Topic.prototype.show = function(){
+}
+
+Topic.prototype.setDisplayType = function(type){
+	this.elt.setAttribute('class', type + ' topic');
 }
