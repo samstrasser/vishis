aad04594f7c6fdb8de699b3a972cd9e16f167f03
sam.strasser <sam.strasser@2eca556a-c63e-0410-9520-154fc19a35bf>
2008-01-11 21:10:20 +0000

    Wrote PHP representation of the data including classes for Topic, Node, SearchResult.  blurb is not yet supported.

--- php/VishisDatabase.php
+++ php/VishisDatabase.php
@@ -1,7 +1,7 @@
 <?
-// todo: check the scope of this
-require_once('dbvars.php');
+require_once('SearchLayer.php');
 
+// todo: check the scope of this
 class VishisDatabase implements TrustedSite{
 	public function __construct(){
 		$this->conn = false;
@@ -10,27 +10,9 @@ class VishisDatabase implements TrustedSite{
 		$this->connect();
 	}
 	public function connect(){
+		require_once('dbvars.php');
 		$this->conn = mysql_connect($host, $user, $pass);
-		$this->db = mysql_select_db($db, $conn);
-	}
-
-	public function search($query){
-		$result = new SearchResult();
-		// check if a topic has the exact title
-		
-		// if there's no exact message, check for similar matches
-		
-		// if there's still no match, return an empty result
-		if(false){
-			return NO_RESULT;
-		}
-		
-		// once there is a match, get all of the children
-		
-		// for each of the children
-			// get all the blurb pieces
-		
-		return $result;
+		$this->db = mysql_select_db($db, $this->conn);
 	}
 
 	public function disconnect(){
@@ -39,7 +21,72 @@ class VishisDatabase implements TrustedSite{
 		}
 	}
 
-}
+	public function search($query){
+		$result = new SearchResult();
+		
+		$topic = $this->getTopic($query);
+		if(!$topic){ return $result; }
+				
+		// once there is a match, get all of the children
+		$children = $this->getChildren($topic);
+		$topic->addChildren($children);
+		
+		$children = array(); // so we don't enter the loop, for now
+		
+		
+		foreach($children as $uid => $child){
+			// get all the blurb pieces
+			$blurbPieces = $this->getBlurb($uid);
+			
+			// add the blurb pieces to the child
+			
+			// add the child to the topic
+		}			
+		
+		$result->addTopic($topic);
+		return $result;
+	}
 
+	private function getTopic($title){
+		$topic = null;
+		
+		// check if a topic has the exact title
+		$q = "select * from nodes where title='$title' limit 1";
+		$r = mysql_query($q, $this->conn);
+		while($row = mysql_fetch_assoc($r)){
+			$topic = new Topic($row);
+		}
+		
+		// if there's no exact message, check for similar matches
+
+		// if there's still no match, return an empty result
+		return $topic;
+	}
+	
+	private function getChildren($topic){
+		$children = array();
+		
+		$uid = $topic->getId();
+		
+		$q = "select * from nodes, node_relations ";
+		$q.= "where nodes.uid = node_relations.to_uid ";
+		$q.= "and node_relations.from_uid = '$uid' ";
+		
+		$r = mysql_query($q, $this->conn);
+		while($row = mysql_fetch_assoc($r)){
+			array_push($children, new Node($row));
+		}
+		return $children;
+	}
+	
+	/**
+	* @param {int} the uid of the node whose blurb to get
+	* @return {Array} the node's blurb, in pieces
+	*/
+	private function getBlurb($nodeId){
+		
+	}
+
+}
 
 ?>
