e2cbacb4f9a64ee6ed80b4afe5fcd82397ae8e9b
sam.strasser <sam.strasser@2eca556a-c63e-0410-9520-154fc19a35bf>
2008-01-11 21:10:20 +0000

    Wrote PHP representation of the data including classes for Topic, Node, SearchResult.  blurb is not yet supported.

--- trunk/php/SearchLayer.php
+++ trunk/php/SearchLayer.php
@@ -5,19 +5,100 @@
  *  
  */
  
+require_once('JSON.php');
+ 
 interface TrustedSite{
-	// Returns a Search Result
+	// @return {SearchResult} result of the query
 	public function search($query);
+	
+	// Assumes the $query exists
+	// public function get($query);
 }
 
 class SearchResult{
 	public function __construct(){
 		$this->json = new Services_JSON();
-		$this->data = array();
+		$this->topics = array();
+	}
+	
+	public function addTopic($t){
+		$this->topics[$t->getId()] = $t;
+	}
+	
+	public function numResults(){
+		return count($this->topics);
 	}
 
 	protected function asJson(){		
 		return $this->json->encode($data);
+	}
+	
+	protected function asHtml(){
+		// pass
+		return "";
+	}
+}
+
+class Node{
+	private $fields = array();
+	private $blurb;
+	
+	public function __construct($fields){
+		foreach($fields as $key => $value){
+			$this->fields[$key] = $value;
+		}
+		
+		$this->blurb = new Blurb();
+	}
+	
+	public function getId(){
+		if(array_key_exists('uid', $this->fields)){
+			return $this->fields['uid'];
+		}else{
+			// todo: error
+			return -1;
+		}
+	}
+}
+
+class Topic extends Node{
+	private $children = array();
+	
+	public function __construct($fields){
+		parent::__construct($fields);
+	}
+	
+	public function addChild(Node $node){
+		$this->children[$node->getId()] = $node;
+	
+	}
+	
+	public function addChildren($children){
+		foreach($children as $child){
+			$this->addChild($child);
+		}
+	}
+}
+
+class Blurb{
+	private $pieces = array();
+}
+
+// Useful for converting between all the formats 
+// e.g. mysql, php, javascript
+class Date{
+	private $pieces = array();
+	
+	public function __construct($dateString, $format='mysql'){
+		$this->dateString = $dateString;
+	}
+	
+	public function asJavascriptString(){
+	
+	}
+	
+	public function asPhpTimestamp(){
+	
 	}
 }
 ?>
