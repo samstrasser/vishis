80b472c2e27a33917a2b242ca1cd05fc7f3bc8e0
sam.strasser <sam.strasser@2eca556a-c63e-0410-9520-154fc19a35bf>
2008-02-14 23:18:29 +0000

    Added initial support for KML files (not including polygons)

--- php/KmlFile.php
+++ php/KmlFile.php
@@ -2,45 +2,109 @@
 require_once('SearchLayer.php');
 
 class KmlFile implements TrustedSite{
+	const tag_topic 	= 'DOCUMENT';
+	const tag_event 	= 'PLACEMARK';
+	const tag_title 	= 'NAME';
+	const tag_start 	= 'BEGIN';
+	const tag_end 		= 'END';
+	const tag_point		= 'POINT';
+	const tag_coords 	= 'COORDINATES';
+
 	public function __construct(){
 		// I don't think there is anything that needs to go here
 	}
 
 	public function search($url){
 		$this->result = new SearchResult();
-		
-/*
-		$topic = new Topic($row);
-		$children[] = new Node($row);
-		*/
 
 		if (!($fp = fopen($url, "r"))) {
 		    die("could not open XML input");
 		}
 
+		$this->depth = array();
+
 		$this->parse($fp);
 		return $this->result;
 	}
 
-	public function startElement($parser, $name, $attrs) {
-	    global $depth;
-	    for ($i = 0; $i < $depth[$parser]; $i++) {
+	private function startElement($parser, $name, $attrs) {
+		/*
+		for ($i = 0; $i < $this->depth[$parser]; $i++) {
 	        echo "  ";
 	    }
 	    echo "$name\n";
-	    $depth[$parser]++;
+	    $this->depth[$parser]++;
+		*/
+	
+		array_push($this->tagStack, $name);
+		
+		if($name == self::tag_topic){
+			$this->currTopic = new Topic();
+		}elseif($name == self::tag_event){
+			$this->currEvent = new Node();			
+		}else{
+			// Remember which tag this is, so when we get data, we can interpret it properly
+			$this->currTag = $name;
+		}
 	}
 
-	public function endElement($parser, $name) {
-	    global $depth;
-	    $depth[$parser]--;
+	private function endElement($parser, $name) {
+		//
+		// $this->depth[$parser]--;
+		//
+		
+		$last = array_pop($this->tagStack);
+		// $last == $name // sanity check
+		
+		if($name == self::tag_topic){
+			$this->result->addTopic($this->currTopic);
+		}elseif($name == self::tag_event){
+			$this->currTopic->addChild($this->currEvent);
+			//$this->currEvent = false;
+		}else{
+			// For now, we are going to ignore all other tags
+		}
 	}
 	
-	public function parse($fp){
+	private function elementData($parser, $data){
+		if($this->currTag == self::tag_title){
+			$title = trim($data);
+			
+			// Is this the Event's title?
+			if(in_array(self::tag_event, $this->tagStack)){
+				$this->currEvent->addField('title', $title);
+			}else{
+				$this->currTopic->addField('title', $title);
+			}
+		}elseif($this->currTag == self::tag_start){
+			$dateString = trim($data);
+			$this->currEvent->addField('start', HistoricalDate::kmlToJs($dateString));
+		}elseif($this->currTag == self::tag_end){
+			$dateString = trim($data);
+			$this->currEvent->addField('end', HistoricalDate::kmlToJs($dateString));
+		}elseif($this->currTag == self::tag_coords){
+			// make sure this is a point ( since we aren't supporting polygons yet)
+			if(in_array(self::tag_point, $this->tagStack)){
+				$coordPair = trim($data);
+				$latlng = explode(',', $coordPair);
+				$this->currEvent->addField('lat', $latlng[0]);
+				$this->currEvent->addField('lng', $latlng[1]);
+			}
+		}else{
+			// For now, we are going to ignore all other tags
+		}
+	}
+	
+	private function parse($fp){
+		$this->tagStack = array();
+		
 		$parser = xml_parser_create();
 		xml_set_element_handler($parser, 
 								array($this, "startElement"), 
 								array($this, "endElement")
+								);
+		xml_set_character_data_handler($parser, 
+								array($this, "elementData")
 								);
 
 		while ($data = fread($fp, 4096)) {
