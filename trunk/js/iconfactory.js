6f5c016d0e032c952a151e1349fae3348392062b
sam.strasser <sam.strasser@2eca556a-c63e-0410-9520-154fc19a35bf>
2008-04-05 21:00:40 +0000

    Midway through doing all of the nav panel styles.  About to start on the add/remove buttons.

--- trunk/js/iconfactory.js
+++ trunk/js/iconfactory.js
@@ -31,8 +31,9 @@ IconFactory.createMarkerIcon = function(opts) {
   var cornerColor = opts.cornerColor || "#ffffff";
    
   var baseUrl = "http://chart.apis.google.com/chart?cht=mm";
-  var iconUrl = baseUrl + "&chs=" + width + "x" + height + 
-      "&chco=" + cornerColor.replace("#", "") + "," + primaryColor.replace("#", "") + "," + strokeColor.replace("#", "") + "&ext=.png";
+  
+  var iconUrl = IconFactory.getIconUrl(baseUrl, width, height, cornerColor, primaryColor, strokeColor);
+  
   var icon = new GIcon(G_DEFAULT_ICON);
   icon.image = iconUrl;
   icon.iconSize = new GSize(width, height);
@@ -62,4 +63,24 @@ IconFactory.createMarkerIcon = function(opts) {
   }
 
   return icon;
+}
+
+IconFactory.getIconUrlByOpts = function(opts){
+  var width = opts.width || 16;
+  var height = opts.height || 16;
+  var primaryColor = opts.primaryColor || "#ff0000";
+  var strokeColor = opts.strokeColor || "#000000";
+  var cornerColor = opts.cornerColor || "#ffffff";
+   
+  var baseUrl = "http://chart.apis.google.com/chart?cht=mm";
+  
+  var iconUrl = IconFactory.getIconUrl(baseUrl, width, height, cornerColor, primaryColor, strokeColor);
+  
+  return IconFactory.getIconUrl(baseUrl, width, height, cornerColor, primaryColor, strokeColor);
+}
+
+IconFactory.getIconUrl = function(baseUrl, width, height, cornerColor, primaryColor, strokeColor){
+	var iconUrl = baseUrl + "&chs=" + width + "x" + height + 
+      "&chco=" + cornerColor.replace("#", "") + "," + primaryColor.replace("#", "") + "," + strokeColor.replace("#", "") + "&ext=.png";
+	return iconUrl;
 }
